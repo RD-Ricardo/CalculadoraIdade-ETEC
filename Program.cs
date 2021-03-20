1bd2f4bf28f29385822f1cba568dfeb55f43cb64
RD-Ricardo <ricardodias169@gmail.com>
2021-03-20 19:31:43 -0300

    Finalizando Programa

--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@ namespace CalculadoraIdade
         static void Main(string[] args)
         {   
             //Data  de nascimentos
-            DateTime  age = new DateTime(2004, 08 , 04).Date;
-            string date = age.ToShortDateString();
-            DateTime now = DateTime.Now;
+            DateTime tempBirthDate = new DateTime(2004, 08 , 04).Date;
+            string date = tempBirthDate.ToShortDateString();
+            //Data de Agora
+            DateTime dateNow = DateTime.Now;
             // Data de nascimento em dias
-            TimeSpan tempBirth = now - age;
-            int days = tempBirth.Days;
+            TimeSpan tempBirth = dateNow - tempBirthDate;
+            int dateDays = tempBirth.Days;
             Console.WriteLine("---------------------");
             Console.WriteLine("--Calculadora Idade--");
             Console.WriteLine("---------------------");
-            Console.WriteLine($"Eu nasci {date} e já vivi {days} dias.");
+            Console.WriteLine($"Eu nasci {date} e já vivi {dateDays} dias.");
         }
     }
 }
